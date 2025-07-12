316248a93460353d0cb74701270f356c271b05ab
nikolat27 <samalizadeh899@gmail.com>
2025-07-12 21:00:41 +0330

    Changed the salt and hash type to string

--- database/models/files.go
+++ database/models/files.go
@@ -1,6 +1,7 @@
 package models
 
 import (
+	"context"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"time"
@@ -18,11 +19,36 @@ type File struct {
 	Approvable     bool               `json:"approvable" bson:"approvable"`
 	Salt           string             `json:"salt" bson:"salt"`
 	HashedPassword string             `json:"hashed_password" bson:"hashed_password"`
-	MaxDownloads   uint               `json:"max_downloads" bson:"max_downloads"`
+	MaxDownloads   uint64             `json:"max_downloads" bson:"max_downloads"`
 	CreatedAt      time.Time          `json:"created_at" bson:"created_at"`
 	ExpireAt       time.Time          `json:"expire_at" bson:"expire_at"`
 }
 
-func (file *FileModel) CreateFileInstance(ownerId primitive.ObjectID, fileName, address string, salt, hashedPassword []byte, approvable bool, maxDownloads uint64, expireAt time.Time) {
-	
+const FileCollectionName = "files"
+
+func (file *FileModel) CreateFileInstance(ownerId primitive.ObjectID,
+	fileName, address, salt, hashedPassword string, approvable bool,
+	maxDownloads uint64, expireAt time.Time) (primitive.ObjectID, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	newFile := &File{
+		OwnerId:        ownerId,
+		Name:           fileName,
+		Address:        address,
+		Salt:           salt,
+		HashedPassword: hashedPassword,
+		Approvable:     approvable,
+		MaxDownloads:   maxDownloads,
+		CreatedAt:      time.Now(),
+		ExpireAt:       expireAt,
+	}
+
+	id, err := file.DB.Collection(FileCollectionName).InsertOne(ctx, newFile)
+	if err != nil {
+		return primitive.NilObjectID, err
+	}
+
+	return id.InsertedID.(primitive.ObjectID), nil
 }
