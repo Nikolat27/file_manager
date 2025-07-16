0f49194e8d66fa6a86ddb5947d58e336a5acc72e
nikolat27 <samalizadeh899@gmail.com>
2025-07-16 13:45:19 +0330

    Changed the func name to ToObjectID

--- utils/mongo.go
+++ utils/mongo.go
@@ -5,7 +5,7 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-func ConvertStringToObjectID(s string) (primitive.ObjectID, error) {
+func ToObjectID(s string) (primitive.ObjectID, error) {
 	if s == "" {
 		return primitive.NilObjectID, errors.New("given string is empty")
 	}
