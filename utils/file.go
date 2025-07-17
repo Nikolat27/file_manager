5f6519ff6ed8ca96fdff6f1298959078c589c3d7
nikolat27 <samalizadeh899@gmail.com>
2025-07-17 19:52:46 +0330

    Refactored the File uploading and Added Struct with its methods

--- utils/file.go
+++ utils/file.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"crypto/rand"
 	"errors"
-	"fmt"
 	"io"
 	"mime/multipart"
 	"net/http"
@@ -11,61 +10,66 @@ import (
 	"slices"
 )
 
-const (
-	MaxMemoryBytes = 100
-)
-
 var allowedTypes = []string{"image/jpeg", "image/png", "application/zip"}
 
-func UploadFileToDisk(key, userId string, r *http.Request) (string, error) {
-	if err := r.ParseMultipartForm(MaxMemoryBytes << 20); err != nil {
-		return "", err
-	}
-
-	file, handler, err := r.FormFile(key)
-	if err != nil {
-		return "", err
-	}
-	defer func() {
-		if err := file.Close(); err != nil {
-			fmt.Printf(err.Error())
-		}
-	}()
-
-	if err := validateFileFormat(file); err != nil {
-		return "", err
-	}
-
-	uploadDir := "uploads/user_files/" + userId + "/"
-
-	if err := os.MkdirAll(uploadDir, 0755); err != nil {
-		return "", err
-	}
-
-	randomStr := rand.Text()
-	fileName := randomStr + handler.Filename
-
-	dst, err := os.Create(uploadDir + fileName)
-	if err != nil {
-		return "", err
-	}
-	defer func() {
-		if err := dst.Close(); err != nil {
-			fmt.Printf(err.Error())
-		}
-	}()
-
-	if _, err = io.Copy(dst, file); err != nil {
-		return "", err
-	}
-
-	return dst.Name(), nil
+type UploadedFile struct {
+	File multipart.File
+	Size int64
+	Name string
 }
 
-func validateFileFormat(file multipart.File) error {
+func ReadFile(r *http.Request, maxMemory int64) (*UploadedFile, error) {
+	if err := r.ParseMultipartForm(maxMemory); err != nil {
+		return nil, err
+	}
+
+	file, handler, err := r.FormFile("file")
+	if err != nil {
+		return nil, err
+	}
+
+	uploadedFile := &UploadedFile{
+		File: file,
+		Size: handler.Size,
+		Name: handler.Filename,
+	}
+
+	if err := uploadedFile.validateFileFormat(); err != nil {
+		return nil, err
+	}
+
+	return uploadedFile, nil
+}
+
+func (file *UploadedFile) UploadToDisk(userId string) (string, error) {
+	uploadDir := "uploads/user_files/" + userId + "/"
+	if err := makeDir(uploadDir); err != nil {
+		return "", err
+	}
+
+	dst, err := os.Create(uploadDir + file.generateFileName())
+	if err != nil {
+		return "", err
+	}
+	defer dst.Close()
+
+	if _, err = io.Copy(dst, file.File); err != nil {
+		return "", err
+	}
+
+	fileAddress := dst.Name()
+	return fileAddress, nil
+}
+
+func (file *UploadedFile) generateFileName() string {
+	randomStr := rand.Text()
+	return randomStr + file.Name
+}
+
+func (file *UploadedFile) validateFileFormat() error {
 	// Read first 512 bytes
 	header := make([]byte, 512)
-	n, err := file.Read(header)
+	n, err := file.File.Read(header)
 	if err != nil && err != io.EOF {
 		return err
 	}
@@ -76,7 +80,7 @@ func validateFileFormat(file multipart.File) error {
 	}
 
 	// Reset file pointer to the beginning for further reading
-	if seeker, ok := file.(io.Seeker); ok {
+	if seeker, ok := file.File.(io.Seeker); ok {
 		if _, err = seeker.Seek(0, io.SeekStart); err != nil {
 			return err
 		}
@@ -87,11 +91,10 @@ func validateFileFormat(file multipart.File) error {
 	return nil
 }
 
-func DeleteFileFromDisk(address []byte) error {
-	err := os.Remove(string(address))
-	if err != nil {
-		return err
-	}
+func makeDir(uploadDir string) error {
+	return os.MkdirAll(uploadDir, 0755)
+}
 
-	return nil
+func DeleteFileFromDisk(address []byte) error {
+	return os.Remove(string(address))
 }
