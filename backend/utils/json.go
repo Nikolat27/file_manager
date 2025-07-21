cc97699764ce124fa605da9bc06d88cbf5763929
nikolat27 <samalizadeh899@gmail.com>
2025-07-21 18:22:07 +0330

    Deleted the WriteJsonError

--- backend/utils/json.go
+++ backend/utils/json.go
@@ -50,12 +50,3 @@ func WriteError(w http.ResponseWriter, status int, err any) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
-
-func WriteErrorData(w http.ResponseWriter, status int, data interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(status)
-
-	if err := json.NewEncoder(w).Encode(data); err != nil {
-		http.Error(w, "Failed to encode JSON", http.StatusInternalServerError)
-	}
-}
